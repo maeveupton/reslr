fed05c729a72b6ff246e5cf3fb3e741d0569b01b
maeveupton <uptonmaeve010@gmail.com>
2023-03-27 15:41:21 +0100

    update

--- tests/testthat/test-test_reslr_load.R
+++ tests/testthat/test-test_reslr_load.R
@@ -2,22 +2,25 @@
 #
 # library(reslr)
 #
-# # Full dataset test
-# testthat::test_that("simplest example", {
-#   data("NAACproxydata")
-#   data_testsite <- NAACproxydata %>% dplyr::filter(Site == "Cedar Island")
-#   # Load in with reslr_load
-#   reslr_1 <-
-#     reslr_load(
-#       data = NAACproxydata,
-#       n_prediction = 100,
-#       tide_gauge_included = FALSE,
-#       input_Age_type = "CE"
-#     )
-#   testthat::expect_s3_class(reslr_1, "reslr_input")
-#   testthat::expect_true(is.data.frame(reslr_1$data))
-#   testthat::expect_true(is.data.frame(reslr_1$data_grid))
-# })
+# Full dataset test
+testthat::test_that("simplest example", {
+  data("NAACproxydata")
+  data_testsite <- NAACproxydata %>% dplyr::filter(Site == "Cedar Island")
+  # Load in with reslr_load
+  reslr_1 <-
+    reslr_load(data = data_testsite,
+               n_prediction = 100,
+               include_tide_gauge = FALSE,
+               include_linear_rate = FALSE,
+               list_preferred_TGs = NULL,
+               TG_minimum_dist_proxy = FALSE,
+               all_TG_1deg = FALSE,
+               input_Age_type = "CE"
+    )
+  testthat::expect_s3_class(reslr_1, "reslr_input")
+  testthat::expect_true(is.data.frame(reslr_1$data))
+  testthat::expect_true(is.data.frame(reslr_1$data_grid))
+})
 #
 # # Filtering for 1 site to increase speed
 # testthat::test_that("simplest example", {
