3ade03c1d1b80652645a38ad8cd108edaa5c0ba5
maeveupton <uptonmaeve010@gmail.com>
2023-03-29 17:11:00 +0100

    Update test-test_plot.reslr_input.R

--- tests/testthat/test-test_plot.reslr_input.R
+++ tests/testthat/test-test_plot.reslr_input.R
@@ -1,6 +1,6 @@
 co <- function(expr) capture.output(expr, file = "NUL")
 
-# Testing printing functions
+# Testing plotting functions
 data <- NAACproxydata %>% dplyr::filter(Site == "Cedar Island")
 reslr_input_1 <- reslr_load(data = data)
 jags_output_1 <- reslr_mcmc(input_data = reslr_input_1,
@@ -10,18 +10,16 @@ jags_output_1 <- reslr_mcmc(input_data = reslr_input_1,
                             n_thin = 1,
                             n_chains = 1)
 
-# Test printing input
-testthat::test_that("print.reslr_input", {
-  testthat::expect_output(print(reslr_input_1))
-})
-# Test printing output
-testthat::test_that("print.reslr_output", {
-  testthat::expect_output(print(jags_output_1))
-})
-# Test summary function
-test_that("summary.reslr_output", {
-  testthat::expect_output(summary(jags_output_1))
-})
+# testthat::test_that("Basic reslr_input plot", {
+#   p <- plot(reslr_input_1)
+#   #testthat::expect_type(p,"ggplot")
+#   vdiffr::expect_doppelganger("reslr_input", p)
+#   # p <- plot(reslr, colour = FALSE)
+#   # testthat::expect_doppelganger("simmr_input_no_col", p)
+#   # p <- plot(simmr_1, tracers = c(2, 1))
+#   # testthat::expect_doppelganger("simmr_input_rev_tracers", p)
+# })
+
 
 #
 # # Load in with reslr_load
