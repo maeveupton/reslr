663d3dfea6de3dd21cf0ad9ea4317de26667bb78
maeveupton <uptonmaeve010@gmail.com>
2023-04-03 15:41:55 +0100

    update

--- tests/testthat/test-test_plot.reslr_output.R
+++ tests/testthat/test-test_plot.reslr_output.R
@@ -1,4 +1,4 @@
-# co <- function(expr) capture.output(expr, file = "NUL")
+co <- function(expr) capture.output(expr, file = "NUL")
 #
 # # Testing plot result of the simple linear regression for 1 site
 # testthat::test_that("Testing SLR", {
@@ -178,21 +178,92 @@
 #   vdiffr::expect_doppelganger("outputplot1sitenigamTG", p)
 # })
 #
-# # # Testing all loading options for plotting functions for 1 site and tide gauge
-# # reslr_input_2 <- reslr_load(data = data,
-# #                             n_prediction = 100,
-# #                             include_tide_gauge = TRUE,
-# #                             TG_minimum_dist_proxy = TRUE,
-# #                             include_linear_rate = TRUE,
-# #                             input_Age_type = "CE")
-# #
-# # testthat::test_that("Basic reslr_input plot with tide gauges", {
-# #   p2 <- plot(reslr_input_2,
-# #              plot_tide_gauges = TRUE)
-# #   vdiffr::expect_doppelganger("inputplotTG1site", p2)
-# # })
-# #
-# #
+
+# Testing all loading options for plotting functions for 1 site
+data("NAACproxydata")
+data <- NAACproxydata %>% dplyr::filter(Site == "Cedar Island")
+reslr_input_1 <- reslr_load(data = data,
+                            n_prediction = 100,
+                            include_tide_gauge = FALSE,
+                            TG_minimum_dist_proxy = FALSE,
+                            input_Age_type = "CE")
+# Testing EIV SLR
+  jags_output_slr <- reslr_mcmc(
+    input_data = reslr_input_1,
+    model_type = "eiv_slr_t",
+    n_iterations = 10,
+    n_burnin = 1,
+    n_thin = 1,
+    n_chains = 1
+  )
+
+testthat::test_that("Basic reslr_output plot for SLR", {
+  p1 <- plot(jags_output_slr)
+  vdiffr::expect_doppelganger("outputplot1siteSLR", p1)
+})
+
+# Testing EIV CP 1
+  jags_output_cp1 <- reslr_mcmc(
+    input_data = reslr_input_1,
+    model_type = "eiv_cp_t",
+    n_iterations = 10,
+    n_burnin = 1,
+    n_thin = 1,
+    n_chains = 1,
+    n_cp = 1
+  )
+
+testthat::test_that("Basic reslr_output plot for EIV cp 1", {
+  p2 <- plot(jags_output_cp1)
+  vdiffr::expect_doppelganger("outputplot1sitecp1", p2)
+})
+
+# Testing EIV CP 2
+  jags_output_cp2 <- reslr_mcmc(
+    input_data = reslr_input_1,
+    model_type = "eiv_cp_t",
+    n_iterations = 10,
+    n_burnin = 1,
+    n_thin = 1,
+    n_chains = 1,
+    n_cp = 2
+  )
+testthat::test_that("Basic reslr_output plot for EIV cp 2", {
+    p3 <- plot(jags_output_cp2)
+    vdiffr::expect_doppelganger("outputplot1sitecp2", p3)
+  })
+
+# # Testing EIV IGP
+#   jags_output_igp <- reslr_mcmc(
+#     input_data = reslr_input_1,
+#     model_type = "eiv_igp_t",
+#     n_iterations = 10,
+#     n_burnin = 1,
+#     n_thin = 1,
+#     n_chains = 1
+#   )
+#
+# testthat::test_that("Basic reslr_output plot for EIV IGP", {
+#   p4 <- plot(jags_output_igp)
+#   vdiffr::expect_doppelganger("outputplot1siteIGP", p4)
+# })
+#
+# # Testing NI spline t
+# jags_output_nisplinet <- reslr_mcmc(
+#   input_data = reslr_input_1,
+#   model_type = "ni_spline_t",
+#   n_iterations = 10,
+#   n_burnin = 1,
+#   n_thin = 1,
+#   n_chains = 1
+# )
+#
+# testthat::test_that("Basic reslr_output plot for NI spline in t", {
+#   p5 <- plot(jags_output_nisplinet)
+#   vdiffr::expect_doppelganger("outputplot1siteNIsplinet", p5)
+# })
+#
+#
 # # # Testing plotting functions for multiple sites
 # # multidata <- NAACproxydata %>% dplyr::filter(Site %in% c("Cedar Island","Barn Island","Nassau"))
 # # reslr_input_3 <- reslr_load(data = multidata,
