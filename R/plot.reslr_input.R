c9b7f4e0da222b041353c2bcb043a9fdbe4dae90
maeveupton <uptonmaeve010@gmail.com>
2023-04-12 14:00:23 +0100

    update

--- R/plot.reslr_input.R
+++ R/plot.reslr_input.R
@@ -44,44 +44,10 @@ plot.reslr_input <- function(x,
   # Plots
   if (plot_tide_gauges == TRUE) {
     # Plotting both tide gauge and proxy record
-    p_proxy <- ggplot2::ggplot() +
-      ggplot2::geom_rect(data = subset(data,data_type_id =="ProxyData"),
-                         ggplot2::aes(
-        xmin = Age * 1000 - Age_err * 1000, xmax = Age * 1000 + Age_err * 1000,
-        ymin = RSL - RSL_err, ymax = RSL + RSL_err,
-        fill = "gray",
-      ), alpha = 0.7) +
-      ggplot2::geom_point(
-        data = subset(data,data_type_id =="ProxyData"),
-        ggplot2::aes(y = RSL, x = Age * 1000, colour = "black"), size = 0.3
-      ) +
-      ggplot2::labs(x = xlab, y = ylab, title = title) +
-      ggplot2::theme_bw() +
-      ggplot2::labs(colour = "") +
-      ggplot2::theme(
-        strip.text.x = ggplot2::element_text(size = 7),
-        strip.background = ggplot2::element_rect(fill = c("white"))
-      ) +
-      ggplot2::scale_fill_manual("",
-        values = "grey",
-        labels = expression(paste("1-sigma Error")),
-        guide = ggplot2::guide_legend(override.aes = list(alpha = 0.7))
-      ) +
-      ggplot2::scale_colour_manual(
-        values = c("black"),
-        labels = c("Data")
-      ) +
-      ggplot2::facet_wrap(~SiteName) +
-      ggplot2::theme(legend.box = "horizontal", legend.position = "bottom") +
-      ggplot2::guides(color = ggplot2::guide_legend(override.aes = list(size = 3))) +
-      ggplot2::theme(
-        plot.title = ggplot2::element_text(size = 18, face = "bold"),
-        axis.title = ggplot2::element_text(size = 12, face = "bold"),
-        legend.text = ggplot2::element_text(size = 10)
-      )
-    p_tide_gauge <- ggplot2::ggplot() +
-      ggplot2::geom_rect(data = data, ggplot2::aes(
-        xmin = Age * 1000 - Age_err * 1000, xmax = Age * 1000 + Age_err * 1000,
+    p <- ggplot2::ggplot() +
+      ggplot2::geom_rect(data = data,
+                         ggplot2::aes(xmin = Age * 1000 - Age_err * 1000,
+                                      xmax = Age * 1000 + Age_err * 1000,
         ymin = RSL - RSL_err, ymax = RSL + RSL_err,
         fill = "gray",
       ), alpha = 0.7) +
@@ -113,7 +79,79 @@ plot.reslr_input <- function(x,
         axis.title = ggplot2::element_text(size = 12, face = "bold"),
         legend.text = ggplot2::element_text(size = 10)
       )
-    p <- ggpubr::ggarrange(p_proxy,p_tide_gauge)
+
+    # p_proxy <- ggplot2::ggplot() +
+    #   ggplot2::geom_rect(data = subset(data,data_type_id =="ProxyRecord"),
+    #                      ggplot2::aes(
+    #     xmin = Age * 1000 - Age_err * 1000, xmax = Age * 1000 + Age_err * 1000,
+    #     ymin = RSL - RSL_err, ymax = RSL + RSL_err,
+    #     fill = "gray",
+    #   ), alpha = 0.7) +
+    #   ggplot2::geom_point(
+    #     data = subset(data,data_type_id =="ProxyRecord"),
+    #     ggplot2::aes(y = RSL, x = Age * 1000, colour = "black"), size = 0.3
+    #   ) +
+    #   ggplot2::labs(x = xlab, y = ylab, title = title) +
+    #   ggplot2::theme_bw() +
+    #   ggplot2::labs(colour = "") +
+    #   ggplot2::theme(
+    #     strip.text.x = ggplot2::element_text(size = 7),
+    #     strip.background = ggplot2::element_rect(fill = c("white"))
+    #   ) +
+    #   ggplot2::scale_fill_manual("",
+    #     values = "grey",
+    #     labels = expression(paste("1-sigma Error")),
+    #     guide = ggplot2::guide_legend(override.aes = list(alpha = 0.7))
+    #   ) +
+    #   ggplot2::scale_colour_manual(
+    #     values = c("black"),
+    #     labels = c("Data")
+    #   ) +
+    #   ggplot2::facet_wrap(~SiteName) +
+    #   ggplot2::theme(legend.box = "horizontal", legend.position = "bottom") +
+    #   ggplot2::guides(color = ggplot2::guide_legend(override.aes = list(size = 3))) +
+    #   ggplot2::theme(
+    #     plot.title = ggplot2::element_text(size = 18, face = "bold"),
+    #     axis.title = ggplot2::element_text(size = 12, face = "bold"),
+    #     legend.text = ggplot2::element_text(size = 10)
+    #   )
+    # p_tide_gauge <- ggplot2::ggplot() +
+    #   ggplot2::geom_rect(data = subset(data,data_type_id =="TideGaugeData"),
+    #                       ggplot2::aes(
+    #     xmin = Age * 1000 - Age_err * 1000, xmax = Age * 1000 + Age_err * 1000,
+    #     ymin = RSL - RSL_err, ymax = RSL + RSL_err,
+    #     fill = "gray",
+    #   ), alpha = 0.7) +
+    #   ggplot2::geom_point(data = subset(data,data_type_id =="TideGaugeData"),
+    #     ggplot2::aes(y = RSL, x = Age * 1000, colour = "black"), size = 0.3
+    #   ) +
+    #   ggplot2::labs(x = xlab, y = ylab, title = title) +
+    #   ggplot2::theme_bw() +
+    #   ggplot2::labs(colour = "") +
+    #   ggplot2::theme(
+    #     strip.text.x = ggplot2::element_text(size = 7),
+    #     strip.background = ggplot2::element_rect(fill = c("white"))
+    #   ) +
+    #   ggplot2::scale_fill_manual("",
+    #                              values = "grey",
+    #                              labels = expression(paste("1-sigma Error")),
+    #                              guide = ggplot2::guide_legend(override.aes = list(alpha = 0.7))
+    #   ) +
+    #   ggplot2::scale_colour_manual(
+    #     values = c("black"),
+    #     labels = c("Data")
+    #   ) +
+    #   ggplot2::facet_wrap(~SiteName,scales = "free") +
+    #   ggplot2::theme(legend.box = "horizontal", legend.position = "bottom") +
+    #   ggplot2::guides(color = ggplot2::guide_legend(override.aes = list(size = 3))) +
+    #   ggplot2::theme(
+    #     plot.title = ggplot2::element_text(size = 18, face = "bold"),
+    #     axis.title = ggplot2::element_text(size = 12, face = "bold"),
+    #     legend.text = ggplot2::element_text(size = 10)
+    #   )
+    #
+    # p <- p_proxy + p_tide_gauge
+    # #p <- p_tide_gauge
   }
   else {
     data <- data %>%
@@ -156,5 +194,5 @@ plot.reslr_input <- function(x,
   }
 
 
-  return(p) #     print(g)invisible(g)
+  return(p)
 }
---
+++ vignettes/reslr.R
@@ -0,0 +1,64 @@
+## ---- include = FALSE---------------------------------------------------------
+# WHATS this
+knitr::opts_chunk$set(
+  collapse = TRUE,
+  comment = "#>",
+  fig.path = "vig/"
+)
+options(rmarkdown.html_vignette.check_title = FALSE)
+#library(tidyverse)
+
+## ----eval = FALSE,message=FALSE-----------------------------------------------
+#  #install.packages("reslr")
+#  #library(devtools)
+#  #devtools::install()
+#  devtools::install_github("maeveupton/reslr")
+
+## ---- readpkg,eval = TRUE, message=FALSE--------------------------------------
+library(reslr)
+
+## ---- example1site,eval = TRUE------------------------------------------------
+# For 1 site
+data_1site <- reslr::NAACproxydata %>% dplyr::filter(Site == "Cedar Island")
+# For multiple sites
+data_multisite <- reslr::NAACproxydata %>% dplyr::filter(Site %in% c(
+  "Snipe Key", "Cheesequake",
+  "Placentia", "Leeds Point"
+))
+
+## ---- example1dataagain, eval = TRUE------------------------------------------
+example_data_set <- reslr::NAACproxydata 
+# For 1 site
+CedarIslandNC <- example_data_set %>% dplyr::filter(Site == "Cedar Island")
+
+## ---- loadslr, eval = TRUE----------------------------------------------------
+CedarIslandNC <- reslr_load(data = CedarIslandNC,
+                                  include_tide_gauge = FALSE,
+                                  include_linear_rate = FALSE,
+                                  TG_minimum_dist_proxy = FALSE,
+                                  list_preferred_TGs = NULL,
+                                  all_TG_1deg = FALSE,
+                                  prediction_interval = 20,
+                                  input_Age_type = "CE")
+
+## ---- slrdata,eval=TRUE-------------------------------------------------------
+data <- CedarIslandNC$data
+
+## ---- datagridslr, eval = TRUE------------------------------------------------
+data_grid <- CedarIslandNC$data_grid
+
+## ---- printdata, eval=TRUE----------------------------------------------------
+print(CedarIslandNC)
+
+## ---- plotdata,fig.align = 'center',fig.width = 7,fig.height = 5,eval = TRUE----
+plot(CedarIslandNC)
+
+## ----plotdataextended,fig.align = 'center',fig.width = 7,fig.height = 5,eval = TRUE----
+plot(
+  x = CedarIslandNC,
+  title = "Plot of the raw data",
+  xlab = "Age (CE)",
+  ylab = "Relative Sea Level (m)",
+  plot_tide_gauges = FALSE
+)
+
