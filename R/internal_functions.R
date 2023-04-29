d0d47d52a8daa1a16f1ed8d5ffa5862a2e191f3e
maeveupton <uptonmaeve010@gmail.com>
2023-04-29 17:01:12 +0100

    Update internal_functions.R

--- R/internal_functions.R
+++ R/internal_functions.R
@@ -2010,8 +2010,8 @@ bs_bbase_t <- function(x,
 #' @noRd
 # Basis function approach
 bs_bbase_st <- function(x,
-                       xl, #= xl,#min(x),
-                       xr,# = xr,#max(x),
+                       xl=min(x),#xl
+                       xr=max(x),#xr
                        deg = 3,
                        spline_nseg_st = NULL,
                        data = data) {
