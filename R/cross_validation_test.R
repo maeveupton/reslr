6db477a2d019939b0482d8de7f9266fe47280955
maeveupton <uptonmaeve010@gmail.com>
2023-05-26 19:32:37 +0100

    update

--- R/cross_validation_test.R
+++ R/cross_validation_test.R
@@ -1,7 +1,8 @@
 #' This function examines the number of knots used to create the spline model fit.
 #'
 #'
-#' @param object Output object from the \code{\link{reslr_mcmc}}
+#' @param object Output object from the \code{\link{reslr_load}}
+#' @param
 #' @param ... Not in use
 #'
 #' @return A list containing convergence diagnostics and parameter estimates for the output.
@@ -10,9 +11,9 @@
 #' @examples
 #' data <- NAACproxydata %>% dplyr::filter(Site == "Cedar Island")
 #' input_data <- reslr_load(data = data)
-#' jags_output <- reslr_mcmc(input_data = input_data, model_type = "ni_spline_t")
-#' cross_validation_test(object = jags_output)
+#' cross_validation_test(object = input_data)
 cross_validation_test <- function(object,
+                                  spline
                                   ...){
 
 }
