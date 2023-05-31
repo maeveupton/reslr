2aaf89906ad613ff1c129d4a34c80560647322b3
maeveupton <uptonmaeve010@gmail.com>
2023-05-31 16:35:20 +0100

    update

--- tests/testthat/test-cross_val_check.R
+++ tests/testthat/test-cross_val_check.R
@@ -1,19 +1,19 @@
-# # Testing the cross validation function for ni spline in time for 1 site
-# data("NAACproxydata")
-# data <- NAACproxydata %>% dplyr::filter(Site == "Cedar Island")
-# testthat::test_that("Testing cross validation", {
-#   # Cross Validation test
-#   cv <- cross_val_check(raw_data = data,
-#                         model_type ="ni_spline_t",
-#                         n_iterations = 1000,
-#                         n_burnin = 1,
-#                         n_thin = 1,
-#                         n_chains = 1,
-#                         n_fold = 2)
-#
-#
-#   testthat::expect_true(is.list(cv))
-#   testthat::expect_true(is.data.frame(cv$CV_model_df))
-#   testthat::expect_true(is.numeric(cv$total_coverage))
-# })
+# Testing the cross validation function for ni spline in time for 1 site
+data("NAACproxydata")
+data <- NAACproxydata %>% dplyr::filter(Site == "Cedar Island")
+testthat::test_that("Testing cross validation", {
+  # Cross Validation test
+  cv <- cross_val_check(raw_data = data,
+                        model_type ="ni_spline_t",
+                        n_iterations = 10,
+                        n_burnin = 1,
+                        n_thin = 1,
+                        n_chains = 1,
+                        n_fold = 2)
+
+
+  testthat::expect_true(is.list(cv))
+  testthat::expect_true(is.data.frame(cv$CV_model_df))
+  testthat::expect_true(is.numeric(cv$total_coverage))
+})
 
