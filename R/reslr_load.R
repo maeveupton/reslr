be63e05cdd862ec38474038996ffec79a8d0ba0e
maeveupton <uptonmaeve010@gmail.com>
2023-06-14 16:45:11 +0100

    Update reslr_load.R

--- R/reslr_load.R
+++ R/reslr_load.R
@@ -264,7 +264,7 @@ reslr_load <- function(data,
     dplyr::group_by(SiteName) %>%
     dplyr::slice_min(Age) %>%
     dplyr::reframe(
-      # Test
+      # Testing
       min_Age = Age
       #min_Age = ifelse(Age>0, Age - Age_err,Age - Age_err)
     )
